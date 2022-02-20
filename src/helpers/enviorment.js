31977b972dcded828925bade9cfd6543f8e6ac71
Michelle Owsley <michowsley@gmail.com>
2022-02-19 19:18:47 -0500

    idk what going on

--- src/helpers/enviorment.js
+++ src/helpers/enviorment.js
@@ -1,13 +1,13 @@
 let APIURL = " ";
 
-switch(window.location.localhost) {
+switch(window.location.hostname) {
     case 'localhost' || '127.0.0.1':
 
     APIURL = "http://localhost:3000";
     break;
 
     case 'https://maogrindhouse.herokuapp.com':
-        APIURL = "https://mao-grindhouse-server.herokuapp.com"
+        APIURL = "https://mao-gindhouseserver.herokuapp.com"
 }
 
 export default APIURL;
